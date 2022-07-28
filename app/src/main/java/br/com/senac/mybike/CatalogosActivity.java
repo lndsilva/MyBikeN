fb9de168069b56b251f4ec48eb737c8ef1a6d4e3
lndsilva <lndsilva@hotmail.com>
2022-07-28 15:03:43 -0300

    criando os itens da lista

--- app/src/main/java/br/com/senac/mybike/CatalogosActivity.java
+++ app/src/main/java/br/com/senac/mybike/CatalogosActivity.java
@@ -2,6 +2,7 @@ package br.com.senac.mybike;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -21,24 +22,32 @@ public class CatalogosActivity extends AppCompatActivity {
 
         idRecCatalogo = findViewById(R.id.idRecCatalogo);
 
-        //Lista das bicicletas
-
-        listabikes.add(
-                new Bike(
-                        "Ciclo bike",
-                        "2.500,00",
-                        50,
-                        R.drawable.bike
-                )
-        );
-
-
+        listabikes = new ArrayList<>();
+        //Criando a lista de bicicletas
+        //instanciar e inserir os valores das bikes
+        listabikes.add(new Bike("Bike Classica", "R$ 4.500,00", 45, R.drawable.bicke_check));
+        listabikes.add(new Bike("Bike Ckeck", "R$ 2.350,00", 9, R.drawable.bicke_check));
+        listabikes.add(new Bike("Bike Adviatica", "R$ 3.135,00", 12, R.drawable.bike_adviatica));
+        listabikes.add(new Bike("Bike Bags", "R$ 8.322,50", 2, R.drawable.bike_bags));
+        listabikes.add(new Bike("Bike Elétrica", "R$ 14.200,00", 3, R.drawable.bike_eletrica));
+        listabikes.add(new Bike("Bike Gece", "R$ 1.150,00", 30, R.drawable.bike_gece));
+        listabikes.add(new Bike("Bike Henk", "R$ 5.500,00", 10, R.drawable.bike_henk));
+        listabikes.add(new Bike("Bike Passion", "R$ 6.635,00", 6, R.drawable.bike_passion));
+        listabikes.add(new Bike("Bike Severino", "R$ 5.325,20", 7, R.drawable.bike_severino));
+        listabikes.add(new Bike("Bike Verde Menta", "R$ 8.759,00", 2, R.drawable.bike_verde_menta));
 
         //Instanciar a classe adaptadora
 
-        MyAdapterBike adapterBike = new MyAdapterBike(getApplicationContext(),listabikes);
+        MyAdapterBike adapterBike = new MyAdapterBike(getApplicationContext(), listabikes);
 
-        //Definir o tipo de gerenciador de layout
+        //Definir o tipo de gerenciador de layout para grid
+       idRecCatalogo.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
+
+       //Layout do tipo linear
+        //idRecCatalogo.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        //Deixando a lista mais flexível
+        idRecCatalogo.setHasFixedSize(true);
 
 
         idRecCatalogo.setAdapter(adapterBike);
--- app/src/main/java/br/com/senac/mybike/MyAdapterBike.java
+++ app/src/main/java/br/com/senac/mybike/MyAdapterBike.java
@@ -1,8 +1,11 @@
 package br.com.senac.mybike;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,23 +26,53 @@ public class MyAdapterBike extends RecyclerView.Adapter<MyAdapterBike.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        //Instanciar a janela modelo
+        View view;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        view = inflater.inflate(R.layout.modelo_bike,null);
+
+        //Instanciar a classe ViewHolder
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //Inserir os valores nos componentes do modelo.
+        holder.txtTitulo.setText(lstBike.get(position).getTitulo());
+        holder.txtPreco.setText(lstBike.get(position).getPreco());
+        holder.txtDisp.setText(lstBike.get(position).getDisponivel());
+
+        holder.imgBike.setImageResource(lstBike.get(position).getImagemBike());
+
 
     }
 
+    //Quantos itens a lista vai ter
     @Override
     public int getItemCount() {
-        return 0;
+        return lstBike.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        //Criando as variáveis globais que representam o modelo
+        TextView txtTitulo, txtPreco, txtDisp, txtBotao;
+        ImageView imgBike;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            //Ligando as variáveis do java para os ids do modelo
+            txtTitulo = itemView.findViewById(R.id.txtModeloTituloBike);
+            txtPreco = itemView.findViewById(R.id.txtModeloPrecoBike);
+            txtDisp = itemView.findViewById(R.id.txtModeloDisponivelBike);
+
+            imgBike = itemView.findViewById(R.id.imgModeloBike);
+
+
         }
     }
 
