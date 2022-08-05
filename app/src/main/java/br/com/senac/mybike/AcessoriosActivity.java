35be12705033e2ccc3bcf2f2334a7e9eabc998b0
lndsilva <lndsilva@hotmail.com>
2022-08-05 14:20:29 -0300

    inserindo eventos nos icones

--- app/src/main/java/br/com/senac/mybike/AcessoriosActivity.java
+++ app/src/main/java/br/com/senac/mybike/AcessoriosActivity.java
@@ -1,14 +1,62 @@
 package br.com.senac.mybike;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class AcessoriosActivity extends AppCompatActivity {
+    //Criando as variáveis globais
+    BottomAppBar bottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acessorios_layout);
+
+        //apresentando o xml para o java
+        bottomAppBar = findViewById(R.id.bottomBarAcessorios);
+
+        //criando o clique no navigationicon
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no icone do bottombar",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Criando o clique nos itens do menu bottomBar
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.mAlterar:
+                        Toast.makeText(getApplicationContext(),
+                                "Cliquei em alterar",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mExcluir:
+                        Toast.makeText(getApplicationContext(),
+                                "Cliquei em excluir",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mPesquisar:
+                        Toast.makeText(getApplicationContext(),
+                                "Cliquei em pesquisar",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
     }
 }
