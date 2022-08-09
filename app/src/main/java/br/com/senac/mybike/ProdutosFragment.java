57ab7b3863cd470a9b85a04fc032c62b1004ca7b
lndsilva <lndsilva@hotmail.com>
2022-08-09 14:28:30 -0300

    implementando fragments e texts

--- app/src/main/java/br/com/senac/mybike/ProdutosFragment.java
+++ app/src/main/java/br/com/senac/mybike/ProdutosFragment.java
@@ -9,12 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class ClientesFragment extends Fragment {
+public class ProdutosFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clientes, container, false);
+        return inflater.inflate(R.layout.fragment_produtos, container, false);
     }
 }
