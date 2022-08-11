33a638b39debb460b15e0c446b584f13368faec5
lndsilva <lndsilva@hotmail.com>
2022-08-11 14:57:23 -0300

    implementando o clique no bottomView

--- app/src/main/java/br/com/senac/mybike/ConteudoActivity.java
+++ app/src/main/java/br/com/senac/mybike/ConteudoActivity.java
@@ -1,14 +1,62 @@
 package br.com.senac.mybike;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class ConteudoActivity extends AppCompatActivity {
+    //Declarar as variáveis no java
+    BottomNavigationView bottomNavigationView;
+
+    //Instanciando as janelas (Fragments)
+    BikeFragment bikeFragment = new BikeFragment();
+    ClientesFragment clientesFragment = new ClientesFragment();
+    PreferenciasFragment preferenciasFragment = new PreferenciasFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.conteudo_layout);
+
+        //Apresentar o xml ao java
+        bottomNavigationView = findViewById(R.id.idBottomNavView);
+
+        //Carregamento do fragment default (Bike) na janela de conteudo
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frmContainer, bikeFragment).commit();
+
+        //Criando o clique nos itens do menu BottomView
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //Criar o switch case para acessar o menu
+                switch (item.getItemId()) {
+                    case R.id.mBikes:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frmContainer, bikeFragment).commit();
+                        return true;
+                    case R.id.mClientes:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frmContainer, clientesFragment).commit();
+                        return true;
+                    case R.id.mPref:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frmContainer, preferenciasFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
     }
 }
